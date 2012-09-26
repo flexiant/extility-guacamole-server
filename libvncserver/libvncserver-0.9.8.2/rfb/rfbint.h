d538981afc9726961fc230db43d43a9dcdded6d5
Alex Bligh <alex@flexiant.com>
2012-09-26 10:34:35 +0000

    EXTL-3326: make guacd install

--- libvncserver/libvncserver-0.9.8.2/rfb/rfbint.h
+++ libvncserver/libvncserver-0.9.8.2/rfb/rfbint.h
@@ -2,7 +2,7 @@
 #define _RFB_RFBINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "LibVNCServer 0.9.8.2"
-/* generated using a gnu compiler version gcc-4.5.real (Ubuntu/Linaro 4.5.2-8ubuntu4) 4.5.2 Copyright (C) 2010 Free Software Foundation, Inc. This is free software; see the source for copying conditions. There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. */
+/* generated using a gnu compiler version gcc-4.4.real (Ubuntu 4.4.3-4ubuntu5.1) 4.4.3 Copyright (C) 2009 Free Software Foundation, Inc. This is free software; see the source for copying conditions. There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. */
 
 #include <stdint.h>
 
